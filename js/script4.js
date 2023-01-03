b8d0c8c30ff377f56db7a953a121fee8e34036fa
juwon2023 <121804918+juwon2023@users.noreply.github.com>
2023-01-03 09:42:28 +0900

    Add files via upload

---
+++ js/script4.js
@@ -0,0 +1,35 @@
+$(function(){
+    $(".btn_1").click(function(){
+        $(".popup_1").fadeIn('slow');
+    });
+    $(".popup_1").click(function(){
+        $(".popup_1").fadeOut('slow');
+    });
+});
+
+$(function(){
+    $(".btn_2").click(function(){
+        $(".popup_2").fadeIn('slow');
+    });
+    $(".popup_2").click(function(){
+        $(".popup_2").fadeOut('slow');
+    });
+});
+
+$(function(){
+    $(".btn_3").click(function(){
+        $(".popup_3").fadeIn('slow');
+    });
+    $(".popup_3").click(function(){
+        $(".popup_3").fadeOut('slow');
+    });
+});
+
+$(function(){
+    $(".btn_4").click(function(){
+        $(".popup_4").fadeIn('slow');
+    });
+    $(".popup_4").click(function(){
+        $(".popup_4").fadeOut('slow');
+    });
+});
